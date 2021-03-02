2d6b4ac2d160e0ed1aa7664001b9bba373f81ec4
HF XU <hfxu@HFdeMacBook-Pro.local>
2021-03-02 09:44:01 -0700

    updated login and register ui

---
+++ firstapp/form.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+#from .models import User
+
+class UserForm(UserCreationForm):
+
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        
+        for field_number in self.fields:
+            self.fields[field_number].help_text = None
+            
+    class Meta(UserCreationForm):
+        model = User
+        fields = ['email','username','first_name','last_name','password1', 'password2']
+            
+#https://stackoverflow.com/questions/13202845/removing-help-text-from-django-usercreateform
+#for removing extra help texts
---
+++ firstapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+#from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class UserCreation(models.Model):
+
+    user = models.CharField(max_length=20)
+    #password = models.CharField(max_length=20)
+    github = models.URLField(blank=True)
+    host = models.TextField(max_length=500, blank=True)
+    
+  #  profile = models.ImageField(upload_to='profile', blank=True)
+    
+    def __str__(self):
+        return self.user.username
+    
