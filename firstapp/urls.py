1fb70c78ebc321a618a9ae6b1acaabb3c643ac65
Kyle Androschuk <kandrosc@ualberta.ca>
2021-03-05 01:07:26 -0700

    finished private to other author

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('homepage/', views.homepage, name='home'),
-    path('author/<int:user_id>/posts/<int:post_id>',views.post,name='post'), # Only one user for now: user id 12345
+    path('author/<int:user_id>/posts/<int:post_id>',views.post,name='post'),
     path('author/<int:user_id>/posts/',views.allposts,name='allposts')
 ]
