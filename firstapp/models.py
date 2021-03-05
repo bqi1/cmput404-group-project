fa06af631f46324f5af0a2c6c92992c842ce161f
brian <brian.qi138@gmail.com>
2021-03-04 17:01:08 -0700

    Edited fields, started on hosting images, working on UUID for users

--- firstapp/models.py
+++ firstapp/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
+    type = "author"
     username = models.CharField(max_length=20)
     #password = models.CharField(max_length=20)
     github = models.URLField(blank=True) # Github link
@@ -14,8 +15,9 @@ class Author(models.Model):
     userid = models.PositiveIntegerField(default=0) # Good for finding their URL in posts
     email = models.EmailField(default="example@gmail.com")
     name = models.CharField(max_length=20,default="testname") # First and last name
+    id = models.TextField(max_length=500, blank=True,primary_key=True) # Host/author/unique id
     def __str__(self):
-        return self.user
+        return self.username
 class Setting(models.Model):
   # Contains variables for global settings
   # Should be a singleton setting in Admin. If not set/initialized in admin, error is thrown when trying to sign up.
