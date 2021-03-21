6c7e1a01d57bda24d35fe875fce8e6410873bb2f
xuhf0429 <42719246+xuhf0429@users.noreply.github.com>
2021-03-20 20:41:43 -0600

    Delete is_shared.py

--- firstapp/is_shared.py
+++
@@ -1,7 +0,0 @@
-from .models import Share
-
-def get_share(from_user, to_user):
-    try:
-        return Share.objects.get(from_user=from_user, to_user=to_user)
-    except Share.DoesNotExist:
-        return False
