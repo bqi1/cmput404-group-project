c620ee47e1961f070ce13d9e7ff369b270f1bbde
Jacky Huang <jacky_huang56@hotmail.com>
2021-03-29 18:23:05 -0600

    unlimited liking bug in heroku but not in local???

--- firstapp/views.py
+++ firstapp/views.py
@@ -443,6 +443,8 @@ def likepost(request, user_id, post_id):
     cursor.execute("SELECT * FROM firstapp_postlikes WHERE from_user = %d AND post_id = %d"% (request.user.id, post_id))
     data = cursor.fetchall()
     # if post has already been liked
+    print("\n\n\n\n\n\n\n\n\n"+str(len(data)))
+
     if len(data) > 0:
         return HttpResponse("Post already liked", status=409)
     else:
