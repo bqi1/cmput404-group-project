0a88643143c0b17a1ff517c14c387940f9cc2b39
brian <brian.qi138@gmail.com>
2021-04-10 23:42:56 -0600

    debugging commenting on homepage

--- firstapp/views.py
+++ firstapp/views.py
@@ -744,6 +744,8 @@ def publicposts(request):
                 if request.get_host() in comment_obj.from_user:
                     print("ONE OF US")
                     # http://c404posties.herokuapp.com/author/
+                    print(f"http://{request.get_host()}/author/")
+                    print(comment_obj.from_user[len(f"http://{request.get_host()}/author/")-1:])
                     author = Author.objects.get(consistent_id=comment_obj.from_user[len(f"http://{request.get_host()}/author/")-1:])
                     print(f"GOTEM {author}")
                     from_author_dict = {
