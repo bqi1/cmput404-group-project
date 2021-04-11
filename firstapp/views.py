bdb992ecce2b510aecf1f99693f6a3dff14f83a5
brian <brian.qi138@gmail.com>
2021-04-11 14:53:34 -0600

    fixing viewcomments

--- firstapp/views.py
+++ firstapp/views.py
@@ -874,7 +874,7 @@ def viewComments(request, user_id, post_id):
         comments = Comment.objects.filter(post_id=post_id)
         for comment in comments:
            # for comment in comments:
-            author = Author.objects.get(consistent_id = comment.to_user)
+            author = Author.objects.get(consistent_id = comment_obj.to_user[len(f"http://{request.get_host()}/author/")+1:])
             author_dict = {
                 "type":"author",
                 "id": f"{author.host}/author/{author.consistent_id}",
