9178ede6f5fe21223361787bfea81699e012571b
brian <brian.qi138@gmail.com>
2021-04-11 00:31:01 -0600

    debugging liking on homepage

--- firstapp/views.py
+++ firstapp/views.py
@@ -1163,7 +1163,7 @@ def commentAHomePagePost(request):
     print(post['author']['host'])
     print(request.get_host())
 
-    if post['author']['host'] == request.get_host() or f"https://{request.get_host()}/" == f"{post['author']['host']}" or f"https://{request.get_host()}/" == f"{post['author']['host']}":
+    if post['author']['host'] == request.get_host() or f"http://{request.get_host()}/" == f"{post['author']['host']}" or f"https://{request.get_host()}/" == f"{post['author']['host']}":
         comment = Comment.objects.create(post_id=post["id"],comment_id=f"{post['id']}/comments/{uuid.uuid4().hex}",from_user=f"{author.host}/author/{author.consistent_id}",to_user=post["author"]["id"],comment_text=comment,published=str(datetime.now()))
         comment.save()
         print(f"comment is {comment}")
