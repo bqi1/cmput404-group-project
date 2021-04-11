0e1aafaf0220df4f0f745b23f9c59f3b0ae40eec
Jacky Huang <jacky_huang56@hotmail.com>
2021-04-10 20:30:52 -0600

    making changes to like models

--- firstapp/views.py
+++ firstapp/views.py
@@ -626,8 +626,9 @@ def postlikes(request, user_id, post_id):
     agent = request.META["HTTP_USER_AGENT"]
     is_ajax = request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
     if is_ajax:
-        object = 
-        postlikes = Like.objects.filter(to_user=user_id,object=post_id)
+        host = request.build_absolute_uri()
+        object = f"{host}/author/{user_id}/posts/{post_id}"
+        postlikes = Like.objects.filter(to_user=user_id,object=object)
         data = serializers.serialize('json', postlikes)
         return HttpResponse(data, content_type="application/json")
     else:
